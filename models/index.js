bcb303cc57d95df631d240aec76fc4f7674242ed
Cesar A. Garcia <mrcesar9402@gmail.com>
2016-08-12 17:06:41 -0400

    modelos parcialmente terminados

--- models/index.js
+++ models/index.js
@@ -30,6 +30,15 @@ db.horario.hasOne(db.solicitud,{as: 'solictud', foreignKey: 'horario'});
 db.solicitud.hasOne(db.sesion_tutoria,{as: 'sesion_tutoria', foreignKey: 'solicitud'});
 db.usuario.hasOne(db.tutor, {as: 'tutor', foreignKey: 'usuario'});
 db.tutor.hasOne(db.sesion_tutoria, {as: 'sesion_tutoria', foreignKey: 'tutor'});
+db.usuario.hasOne(db.estudiante, {as: 'tutor', foreignKey: 'usuario'});
+
+db.evaluacion.belongsTo(db.sesion_tutoria, {as: 'evaluaciones', foreignKey: 'sesion_tutoria'});
+db.evaluacion.hasMany(db.opcion_evaluacion, {as: 'opciones', foreignKey: 'evaluacion'});
+db.sesion_tutoria.hasMany(db.recurso_workspace, {as: 'recursos', foreignKey: 'sesion_tutoria'});
+
+db.estudiante.belongsToMany(db.opcion_evaluacion, {as: 'respuestas', through: 'respuesta_evaluacion', foreignKey: 'estudiante'});
+db.opcion_evaluacion.belongsToMany(db.estudiante, {as: 'estudiantes', through: 'respuesta_evaluacion', foreignKey: 'respuesta'});
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
