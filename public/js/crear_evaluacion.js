6435ec52f78f8cded98490f709df163a641758b0
Cesar A. Garcia <mrcesar9402@gmail.com>
2016-08-29 18:52:14 -0400

    adicion dinamica de preguntas en la vista, terminada

--- public/js/crear_evaluacion.js
+++ public/js/crear_evaluacion.js
@@ -7,19 +7,25 @@ $(document).ready(function() {
     var div_respuesta = $("#div_respuesta_dummy").clone().removeAttr('id');
     $("#div_respuesta_dummy").remove();
     var respuestas = $("#div_respuestas");
-    var respuestas_correctas = $("#div_respuesta_correcta");
-    var item_respuesta_correcta = $("#respuesta_correcta_dummy").clone();
+    var respuestas_correctas = $("#select_respuesta");
+    var item_respuesta_correcta = $("#respuesta_correcta_dummy").clone().removeAttr('id');
     $("#respuesta_correcta_dummy").remove();
 
-    var eliminarRespuesta = function (event) {
-        if(cantidad_respuestas <= 2) {
-            //no se puede borrar porque quedaria una sola alternativa
-        }
-        else {
+    var eliminarRespuesta = function (e) {
+        //solo se puede eliminar respuesta si hay mas de 2
+        if(cantidad_respuestas > 2) {
+            cantidad_respuestas--;
             //borrar pregunta correspondiente
+            var resp_div = $(this).parent().parent();
+            resp_div.remove();
             //borrar ultima respuesta del radio button
-                //si esta seleccionada, seleccionar la primera
-                //borrar y ya en cualquier otro caso
+            var last_radio_item = respuestas_correctas.find("option:last");
+            //borrar y ya en cualquier otro caso
+            last_radio_item.remove();
+            //actualizar select de materialize
+            $("#select_respuesta").material_select();
+            //habilitar boton de agregar
+            btn_agregar.attr("disabled",false);
         }
     };
 
@@ -32,15 +38,22 @@ $(document).ready(function() {
             //agregar input de respuesta
             respuestas.append(div_respuesta.clone().removeClass('hide'));
             //agregar radio_button de respuesta correcta
-            var nueva_resp = item_respuesta_correcta.clone().removeClass('hide');
-            nueva_resp.find("input").attr("id","resp_"+cantidad_respuestas).val(cantidad_respuestas);
-            nueva_resp.find("label").attr("for","resp_"+cantidad_respuestas).text("Pregunta #"+cantidad_respuestas);
+            var nueva_resp = item_respuesta_correcta.clone().removeAttr('class');
+            nueva_resp.val(cantidad_respuestas);
+            nueva_resp.text("Pregunta #"+cantidad_respuestas);
             respuestas_correctas.append(nueva_resp);
 
             $("a.btn_eliminar_respuesta:last").click(eliminarRespuesta);
+            $('#select_respuesta').material_select();
+
+            if(cantidad_respuestas >= 10) {
+                btn_agregar.attr("disabled", true);
+            }
         }
         else {
             //bloquear boton de agregar (se libera en otro sitio)
         }
     });
+
+    $('#select_respuesta').material_select();
 });
